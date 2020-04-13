d1b9ab0712464ec2ca59154ff7bfb2f3d8a8e997
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-13 22:19:02 +0200

    Lecture BDD

--- CreeBDD.php
+++ CreeBDD.php
@@ -1,4 +1,31 @@
 <?php 
+	//connexionau a la bdd
+$db_handle = mysqli_connect('localhost', 'root', '');
 
-echo $'adresse';
-  ?>
+	//test de connexion
+if ($db_handle -> connect_errno){
+	echo "Echec connexion";
+	exit();
+}
+
+	//on supprime pour eviter tout doublon 
+mysqli_query($db_handle,"DROP DATABASE IF EXISTS ecey;");
+	// On cree la bdd
+mysqli_query($db_handle,"CREATE DATABASE ecey;");
+
+	//on ouvre le fichier contenant les requettes
+$data=fopen('BDD.sql','r');
+
+if($data){
+	echo "lecture des données impossible";
+	exit();
+}else {
+	//tant que le fichier n'est pas fini on execute ligne par ligne les commandes dans la base de donnée
+	while(!feof($data)){
+		//lecture ligne par ligne
+		$requete=fgets($data)
+		//execution de la requette 
+		mysqli_query($db_handle,$requete);
+	}
+}
+?>
