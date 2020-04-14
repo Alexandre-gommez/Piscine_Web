5a25d2de06934806b80e77b1e31f5967bf713420
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-14 16:51:02 +0200

    Add personne et modif BDD

--- BDD.sql
+++ BDD.sql
@@ -1,14 +1,13 @@
---Table Membre
 --Table personne
-CREATE TABLE Personne(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL,Prenom varchar(50) NOT NULL,Mail varchar(50) NOT NULL,NumTel varchar(10) NOT NULL,Mdp varchar(50) NOT NULL,adresse1 varchar(50) NOT NULL, adresse2 varchar(50),ville varchar(50) NOT NULL,CodePostal int NOT NULL, Pays varchar(50) NOT NULL);
+CREATE TABLE Personne(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL,Prenom varchar(50) NOT NULL,Mail varchar(50) NOT NULL,NumTel varchar(10) NOT NULL,Mdp varchar(50) NOT NULL,adresse1 varchar(50) NOT NULL,adresse2 varchar(50),ville varchar(50) NOT NULL,CodePostal int NOT NULL,Pays varchar(50) NOT NULL,Carte int, FOREIGN KEY(Carte) REFERENCES Carte(Id));
 --Table CB
-CREATE TABLE CB(Nom_Carte varchar(50) NOT NULL,Num_Carte varchar(50) NOT NULL,Crypto int NOT NULL,Type_Carte varchar(50) NOT NULL,Date_Expiration_Carte DATE NOT NULL,Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
+CREATE TABLE CB(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Solde varchar(50) NOT NULL,Nom_Carte varchar(50) NOT NULL,Num varchar(50) NOT NULL,Crypto int NOT NULL,Type varchar(50) NOT NULL,Date_Expiration DATE NOT NULL);
 --Table Admin
 CREATE TABLE Admin(Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
 --Table Vendeur
-CREATE TABLE Vendeur(Argent varchar(50) NOT NULL,Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
+CREATE TABLE Vendeur(Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
 --Table Acheteur
-CREATE TABLE Vendeur(Argent varchar(50) NOT NULL,Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
+CREATE TABLE Acheteur(Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
 --Table Objet
 CREATE TABLE Objet(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL, Prix float NOT NULL,Description text(1000) NOT NULL,Nom1 varchar(50) NOT NULL,Nom2 varchar(50),Nom3 varchar(50),Nom4 varchar(50),Nom5 varchar(50));
 --Table Enchere
--- CreeBDD.php
+++ CreeBDD.php
@@ -1,7 +1,6 @@
 <?php 
 	//connexionau a la bdd
-$db_handle = mysqli_connect('localhost', 'root', '');
-
+$db_handle = mysqli_connect('localhost', 'root','');
 	//test de connexion
 if ($db_handle -> connect_errno){
 	echo "Echec connexion";
@@ -11,8 +10,10 @@ if ($db_handle -> connect_errno){
 	//on supprime pour eviter tout doublon 
 mysqli_query($db_handle,"DROP DATABASE IF EXISTS ecey;");
 	// On cree la bdd
-mysqli_query($db_handle,"CREATE DATABASE ecey;");
+mysqli_query($db_handle,"CREATE DATABASE ecey DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;");
 
+	// se place dans la table
+$db_found = mysqli_select_db($db_handle, "ecey");
 	//on ouvre le fichier contenant les requettes
 $data=fopen('BDD.sql','r');
 
@@ -24,6 +25,8 @@ if(!$data){
 	while(!feof($data)){
 		//lecture ligne par ligne
 		$requete=fgets($data);
+		echo $requete;
+		echo "<br>";
 		//execution de la requette 
 		mysqli_query($db_handle,$requete);
 	}
