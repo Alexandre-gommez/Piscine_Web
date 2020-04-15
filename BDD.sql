94530011eb32d32b93b87a601b195f9537806424
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-15 16:46:38 +0200

    Version Final

--- BDD.sql
+++ BDD.sql
@@ -1,7 +1,7 @@
 --Table CB
 CREATE TABLE CB(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Solde varchar(50) NOT NULL,Nom_Carte varchar(50) NOT NULL,Num varchar(50) NOT NULL,Crypto int NOT NULL,Type varchar(50) NOT NULL,Date_Expiration varchar(50) NOT NULL);
 --Table Objet
-CREATE TABLE Objet(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL, Prix float NOT NULL,Description text(1000) NOT NULL,Image1 varchar(50) NOT NULL,Image2 varchar(50),Image3 varchar(50),Image4 varchar(50),Video varchar(50));
+CREATE TABLE Objet(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL,Description text(1000) NOT NULL,Image1 varchar(50) NOT NULL,Image2 varchar(50),Image3 varchar(50),Image4 varchar(50),Video varchar(50));
 --Table personne
 CREATE TABLE Personne(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Nom varchar(50) NOT NULL,Prenom varchar(50) NOT NULL,Mail varchar(50) NOT NULL,NumTel varchar(10) NOT NULL,Mdp varchar(50) NOT NULL,adresse1 varchar(50) NOT NULL,adresse2 varchar(50),ville varchar(50) NOT NULL,CodePostal int NOT NULL,Pays varchar(50) NOT NULL, Carte int, FOREIGN KEY(Carte) REFERENCES CB(Id),username varchar(50) NOT NULL);
 --Table Admin
@@ -11,11 +11,11 @@ CREATE TABLE Vendeur(Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Per
 --Table Acheteur
 CREATE TABLE Acheteur(Personne int NOT NULL, FOREIGN KEY(Personne) REFERENCES Personne(Id));
 --Table Enchere
-CREATE TABLE Enchere(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id),Fin DATE NOT NULL);
+CREATE TABLE Enchere(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id),Fin DATE NOT NULL, Prix float NOT NULL);
 --Table Achat
-CREATE TABLE Achat(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id));
+CREATE TABLE Achat(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id), Prix float NOT NULL);
 --Table Offre
-CREATE TABLE Offre(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id),Nb int NOT NULL);
+CREATE TABLE Offre(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id),Nb int NOT NULL, Prix float NOT NULL);
 --Table Ferraille
 CREATE TABLE Ferraille(Objet int NOT NULL, FOREIGN KEY(Objet) REFERENCES Objet(Id));
 --Table Mussée
