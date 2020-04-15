1a0ba1f2af6fff341491ccb746bde0ab3c2ed484
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-15 15:38:27 +0200

    Test nouveaux format bdd

--- login.php
+++ login.php
@@ -8,5 +8,5 @@ $mdp=isset($_POST["password"]) ? $_POST["password"]:"";
 $test=mysqli_query($db_handle,"SELECT Id FROM Personne WHERE username ='".$id . "' AND Mdp ='" .$mdp ."'")
 $Personne=mysqli_fetch_assoc($test);
 $idPersonne=$Personne['id'];
-echo 'alerte('.$idPersonne.');';
+echo $idPersonne;
 ?>
