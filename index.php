e5b4e6c7116c543016b79beb8c4d88bd5b6385f6
Julien <julien.tr99@gmail.com>
2020-04-16 01:04:30 +0200

    js in index

--- index.php
+++ index.php
@@ -34,9 +34,12 @@ session_start();
         </button>
         <div class="collapse navbar-collapse" id="main-navigation">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="#">Acceuil</a></li>
-                <li class="nav-item"><a class="nav-link" href="product.php">Produits</a></li>
-                <li class="nav-item"><a class="nav-link" href="#page-footer">Profil</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav1" href="#">Acceuil</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav2" href="product.php">Vendre</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav3" href="#page-footer">Acheter</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav4" href="compte.php">Mon Compte</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav5" href="product.php">Admin</a></li>
+                <li class="nav-item"><a class="nav-link" id="nav6" href="#page-footer">Panier</a></li>
             </ul>
         </div>
     </nav>
@@ -78,7 +81,42 @@ session_start();
       </section>
       <br>
       <br>       
-        
+      <?php $message = "Hello Folks"; ?>
+        <script>
+          var elem1 = document.getElementById("nav1");
+          var elem2 = document.getElementById("nav2");
+          var elem3 = document.getElementById("nav3");
+          var elem4 = document.getElementById("nav4");
+          var elem5 = document.getElementById("nav5");
+          var elem6 = document.getElementById("nav6");
+
+          if((<?php echo $_SESSION['role']?>)=='' || (<?php echo $_SESSION['role']?>)==0)
+          {
+            alert("pas connecté poto");
+            elem2.style.display = 'none';
+            elem5.style.display = 'none';
+            elem6.style.display = 'none';
+            elem4.href="Frontlogin.php";
+          }
+          else if((<?php echo $_SESSION['role']?>)==1)
+          {
+            alert("Acheteur poto");
+            elem2.style.display = 'none';
+            elem5.style.display = 'none';
+          }
+          else if((<?php echo $_SESSION['role']?>)==2)
+          {
+            alert("Vendeur poto");
+            elem5.style.display = 'none';
+            elem6.style.display = 'none';
+          }
+          else if((<?php echo $_SESSION['role']?>)==3)
+          {
+            alert("Admin poto");
+            elem2.style.display = 'none';
+            elem6.style.display = 'none';
+          } 
+          </script>
     </body>
     </html>
 
