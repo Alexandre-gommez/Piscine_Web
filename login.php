5dda73d706ed6fb9054a0c5b94834d1ee36faa15
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-15 13:53:33 +0200

    login

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'loginBDD.php';
+
+$id=isset($_POST["username"]) ? $_POST["username"]:"";
+$mdp=isset($_POST["password"]) ? $_POST["password"]:"";
+
+$test=mysqli_query($db_handle,"SELECT Id FROM Personne WHERE username ='".$id . "' AND Mdp ='" .$mdp ."'")
+$Personne=mysqli_fetch_assoc($test);
+$idPersonne=$Personne['id'];
+echo 'alerte('.$idPersonne.');';
+?>
