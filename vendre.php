c69a25373f54a5ef161e084bd30c2ef2fb8e40df
Victor Thevin <v.thevin@live.fr>
2020-04-17 01:06:41 +0200

    Ajout SVG et début vendre

--- vendre.php
+++ vendre.php
@@ -13,13 +13,15 @@ session_start();
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Arapey&display=swap" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link href="css/landing-page.min.css" rel="stylesheet">
     <script type="text/javascript" src="scripts.js"></script>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
             $('.header').height($(window).height());
         });
     </script>
@@ -51,19 +53,87 @@ session_start();
   <a href="#" class="list-group-item list-group-item-action">Second item</a>
   <a href="#" class="list-group-item list-group-item-action">Third item</a>
 </div>-->
-    <div> <!--ce div est pour un article-->
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4">image</div>
-            <div class="col-sm-4">descirption</div>
-            <div class="col-sm-2"></div>
+<br/>
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+                <a href="#" class="list-group-item list-group-item-action">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm">image</div>
+                            <div class="col-sm">description</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm">prix ???</div>
+                            <div class="col-sm"></div>
+                        </div>
+                    </div>
+
+                </a>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4">prix ???</div>
-            <div class="col-sm-4">supprimer</div>
-            <div class="col-sm-2"></div>
+        <div class="row content">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+                <a href="#" class="list-group-item list-group-item-action">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm">image</div>
+                            <div class="col-sm">description</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm">prix ???</div>
+                            <div class="col-sm"></div>
+                        </div>
+                    </div>
+
+                </a>
+            </div>
         </div>
     </div>
+
+    <footer class="footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4">
+          <span class="copyright">Copyright &copy; Your Website 2019</span>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline quicklinks">
+            <li class="list-inline-item">
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 
 </body>
