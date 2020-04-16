eb342d1ded229c32b06ba789921dd7be6b82d556
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-16 15:16:23 +0200

    Requette BDD

--- affichage.php
+++ affichage.php
@@ -5,23 +5,15 @@
 	$categorie=isset($_POST["categorie"]) ? $_POST["categorie"]:"";
 	$vente=isset($_POST["vente"]) ? $_POST["vente"]:"";
 	
-	if ($categorie==0&&$vente==0){
-		$objets=mysqli_query($db_handle,)
-	}else if ($categorie==0&&$vente==1){
-
-	}else if ($categorie==0&&$vente==2){
-		
-	}else if ($categorie==0&&$vente==3){
-		
-	}else if ($categorie==1&&$vente==0){
-		
-	}else if ($categorie==1&&$vente==1){
-		
+	if ($categorie==1&&$vente==1){
+		$Enchere=mysqli_query($db_handle,"SELECT Objet FROM Enchere");
+		$Achat=mysqli_query($db_handle,"SELECT Objet FROM Achat");
+		$Offre=mysqli_query($db_handle,"SELECT Objet FROM Offre");
 	}else if ($categorie==1&&$vente==2){
-		
+
 	}else if ($categorie==1&&$vente==3){
 		
-	}else if ($categorie==2&&$vente==0){
+	}else if ($categorie==1&&$vente==4){
 		
 	}else if ($categorie==2&&$vente==1){
 		
@@ -29,13 +21,23 @@
 		
 	}else if ($categorie==2&&$vente==3){
 		
-	}else if ($categorie==3&&$vente==0){
+	}else if ($categorie==2&&$vente==4){
 		
 	}else if ($categorie==3&&$vente==1){
 		
 	}else if ($categorie==3&&$vente==2){
 		
 	}else if ($categorie==3&&$vente==3){
+		
+	}else if ($categorie==3&&$vente==4){
+		
+	}else if ($categorie==4&&$vente==1){
+		
+	}else if ($categorie==4&&$vente==2){
+		
+	}else if ($categorie==4&&$vente==3){
+		
+	}else if ($categorie==4&&$vente==4){
 		
 	}
 
