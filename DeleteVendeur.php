1d810570028091af64076a3de6c2390490b175dd
Gommez <alexandre.gommez@edu.ece.fr>
2020-04-17 18:10:29 +0200

    Delete vendeur et objet

--- DeleteVendeur.php
+++ DeleteVendeur.php
@@ -1,9 +1,9 @@
 <?php
 include "loginBDD.php";
-
-$boucle=mysqli_query($db_handle,"SELECT * FROM Histovendeur");
-while($liste=mysqli_fetch_assoc($boucle))
-{
-	
-}
+	$id=$_GET['ID'];
+	$temp=mysqli_query($db_handle,"SELECT * FROM Object WHERE Vendeur='".$id."';");
+	while($liste=mysqli_fetch_assoc($boucle))
+	{
+		
+	}
 ?>
