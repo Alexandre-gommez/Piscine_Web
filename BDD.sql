b55fa00dd7919653ce33ea494b8195e9da10ddea
Victor Thevin <v.thevin@live.fr>
2020-04-18 13:31:19 +0200

    5 personnes (2v, 2 ach, 1 admin)

--- BDD.sql
+++ BDD.sql
@@ -35,15 +35,29 @@ CREATE 	TABLE ListeOffres(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Referance i
 --Table Liste Achat
 CREATE TABLE ListeAchat(Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,Achat int NOT NULL, FOREIGN KEY(Achat) REFERENCES Objet(Id), Client int NOT NULL, FOREIGN KEY(Client) REFERENCES Personne(Id));
 --Remplisage
-INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Thévin","1","1","1","12/20");
-INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Terrier","2","2","2","09/17");
-INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Thévin","Victor","victor.thevin@edu.ece.fr","0669632774","a","5 rue Amélie","Gennevilliers","92230","France","1","VT");
-INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Terrier","Julien","julien.terrier@edu.ece.fr","0658657052","b","10 rue de la Paix","Paris","75002","Fance","2","JT");
-INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,username) VALUES ("Gommez","Alexandre","alexandre.gommez@edu.ece.fr","0678258572","c","6 rue du Docteur Rochefort","Chatou","78400","France","AG");
-INSERT INTO Acheteur(Personne) VALUES ("1");
+--Vendeurs
+--Vendeur 1
+INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Terrier","1","1","1","09/21");
+INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Terrier","Julien","julien.terrier@edu.ece.fr","0658657052","a","10 rue de la Paix","Paris","75002","Fance","1","JT");
+INSERT INTO Vendeur(Personne) VALUES ("1");
+INSERT INTO Histovendeur(Personne) VALUES ("1");
+--Vendeur 2
+INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Martin","2","2","2","06/21");
+INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Martin","Pierre","pierre.dupond@edu.ece.fr","0603445612","b","6 rue du Bournaire","Gennevilliers","92230","France","2","PM");
 INSERT INTO Vendeur(Personne) VALUES ("2");
 INSERT INTO Histovendeur(Personne) VALUES ("2");
-INSERT INTO Admin(Personne) VALUES ("3");
+--Acheteurs
+--Acheteur 1
+INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Thévin","3","3","3","12/20");
+INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Thévin","Victor","victor.thevin@edu.ece.fr","0669632774","c","5 rue Amélie","Gennevilliers","92230","France","3","VT");
+INSERT INTO Acheteur(Personne) VALUES ("3");
+--Acheteur 2
+INSERT INTO	CB(Solde,Nom_Carte,Num,Crypto,Type,Date_Expiration) VALUES ("100","Dupuis","4","4","4","10/20");
+INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,Carte,username) VALUES ("Dupuis","Marc","marc.dupuis@edu.ece.fr","0632552678","d","10 avenue de la République","Chatenay-Malabry","92290","Fance","4","MD");
+INSERT INTO Acheteur(Personne) VALUES ("4");
+--Admin
+INSERT INTO Personne(Nom,Prenom,Mail,NumTel,Mdp,adresse1,ville,CodePostal,Pays,username) VALUES ("Gommez","Alexandre","alexandre.gommez@edu.ece.fr","0678258572","e","6 rue du Docteur Rochefort","Chatou","78400","France","AG");
+INSERT INTO Admin(Personne) VALUES ("5");
 --Objet1
 INSERT INTO Objet(Nom,Description,Image1,Vendeur) VALUES ("Ferraille","5 tonnes de dechets","image1.jpeg","2");
 INSERT INTO Ferraille(Objet) VALUES ("1");
@@ -52,7 +66,7 @@ INSERT INTO Offre(Objet,Prix) VALUES ("1","300");
 --Objet2
 INSERT INTO Objet(Nom,Description,Image1,Vendeur) VALUES ("Tag Heuer Carrera","Belle montre sportive","image2.jpeg","2");
 INSERT INTO VIP(Objet) VALUES ("2");
-INSERT INTO Enchere(Objet,Fin,Prix) VALUES("2","25/04/20-12:00","5000")
+INSERT INTO (Objet,Fin,Prix) VALUES("2","25/04/20-12:00","5000")
 INSERT INTO Achat(Objet,Prix) VALUES ("2","7500");
 --Objet3
 INSERT INTO Objet(Nom,Description,Image1,Vendeur) VALUES ("Casque de la guerre","Objet d'epoque","image3.jpeg","2");
